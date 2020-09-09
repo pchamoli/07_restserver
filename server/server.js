b6786808bf8f41093fa76ef8506137ff4d173e3f
pchamoli <pchamoli@hotmail.com>
2020-09-08 23:20:52 -0500

    MEnsaje

--- server/server.js
+++ server/server.js
@@ -1,40 +1,24 @@
 require ('./config/config')
 
 const express = require('express');
+const mongoose = require('mongoose')
+
+const bodyParser = require('body-parser');
+
 const app = express();
-const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    
-    res.json('get usuario');
-})
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function(req, res) {
+mongoose.connect(process.env.URLDB, 
+    {useNewUrlParser: true, useCreateIndex: true},
+    (err, resp) => {
+    if (err) throw err
 
-    let body = req.body;
-
-    if(body.nombre === undefined)
-        res.status(400).json({
-            ok:false,
-            mensaje: "El nombre es necesario"
-        })
-    else
-        res.json({body});
-})
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id: id
-    });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('get usuario');
+    console.log('Base de Datos ONLINE')
 })
 
 app.listen(process.env.PORT, () => {
